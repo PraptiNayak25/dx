9d309fb47f0cf408541b706cb93f41903f2b67e5
prapticnayak@gmail.com <prapticnayak@gmail.com>
2025-05-08 14:38:23 +0530

    save to main

---
+++ abcd.java
@@ -0,0 +1,18 @@
+class Circle {
+    void draw() {
+        System.out.println("Drawing a circle");
+    }
+}
+
+
+
+public class abcd {
+    public static void main(String[] args) {
+        Circle c = new Circle();
+        
+
+        c.draw();
+       
+    }
+}
+
